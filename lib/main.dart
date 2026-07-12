c470ccb9e5545e8ffdf2233a17ef5d7a79809047
zobayersv <nayem.bdcit@gmail.com>
2026-07-12 16:35:20 +0600

    package setup on flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
